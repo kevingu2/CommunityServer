c4e31ff5830d313deb423ac92595f32e57ca07f4
Kevin Gu <kevin_gu@apple.com>
2017-12-18 01:13:48 -0800

    Fix configs

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,8 @@
 /**
  * Config var for app
  **/
+require('dotenv').config({path: '.env.' + process.env.NODE_ENV});
+
 module.exports = {
     port: process.env.PORT || 3000,
     secret: process.env.SECRET || 'mysecret'
